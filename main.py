60b2d5e0cb56c97ae9ca9ce17b33e08a48e5b3a2
MrArtemV <ves.artem2005@gmail.com>
2022-02-20 22:05:35 +0300

    add "lrandom" command (rand it for 1 to 100), add .gitignore file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from config import settings
+import random
 
 bot = commands.Bot(command_prefix=settings["prefix"])
 
@@ -10,5 +11,10 @@ async def hello(ctx):
     author = ctx.message.author
     await ctx.send(f"Hello, {author.mention}!")
 
+
+@bot.command()
+async def lrandom(ctx):
+    text = "your random number:" + str(random.randint(1, 100))
+    await ctx.send(text)
 
 bot.run(settings['token'])
