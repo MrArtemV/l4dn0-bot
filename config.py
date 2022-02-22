a841558da2ef8440f8cd0509c2066ca1f80e60ce
MrArtemV <ves.artem2005@gmail.com>
2022-02-22 18:53:22 +0300

    and NH parser (finally)

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'token': '#',  # Enter token here
-    'bot': '#',  # Bot name
-    'id': 0,  # Enter bot ID here (int, not str)
-    'prefix': '/'  # Default prefix
-}
--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import discord
 from discord.ext import commands
 from config import settings
+from imageparser import ImageLinkHTMLParser
 import random
+import requests
 
 bot = commands.Bot(command_prefix=settings["prefix"])
 
 
+@bot.event
+async def on_ready():
+    print(f"Logged as {bot.user}")
+
+
 @bot.command()
-async def hello(ctx):
+async def hello(ctx, extend=""):
     author = ctx.message.author
-    await ctx.send(f"Hello, {author.mention}!")
+    await ctx.send(f"Hello, {author.mention}! {extend}")
 
 
 @bot.command()
 async def lrandom(ctx):
     text = "your random number:" + str(random.randint(1, 100))
     await ctx.send(text)
+
+
+@bot.command()
+async def nh(ctx, manga_id, number=1):
+    r = requests.get(f"http://nhentai.net/g/{str(manga_id)}")
+    parser = ImageLinkHTMLParser()
+    parser.feed(r.text)
+    linklist = parser.linklist
+    embed = discord.Embed(color=0x1fa398, title=f"страница №{number}, манга {manga_id}")
+    embed.set_image(url=linklist[number-1])
+    await ctx.send(embed=embed)
+
 
 bot.run(settings['token'])
